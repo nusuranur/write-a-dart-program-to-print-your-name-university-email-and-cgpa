81f35f52d3c82165af86aa14394893107c7f7d23
Nusura Nur Nowrin <124068277+nusuranur@users.noreply.github.com>
2024-10-11 15:08:06 +0600

    home_page.dart

---
+++ home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Home Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String name = "Nusura Nur Nowrin";
+  final String university = "Internatioal Islamic University Chittagong";
+  final String email = "nusuranurnowrin@gmail.com";
+  final double cgpa = 3.00;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Personal Details"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Name: $name',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              'University: $university',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              'Email: $email',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              'CGPA: $cgpa',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
